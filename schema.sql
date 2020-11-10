0d4c9899031534d1b69feb378d3c6ffcba6fb43a
fuzzysearch404 <roberts.ziedins@inbox.lv>
2020-11-10 21:59:37 +0200

    Edit schema for the latest database changes

--- schema.sql
+++ schema.sql
@@ -5,39 +5,40 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO disc
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO discordfarm;
 
 CREATE TABLE IF NOT EXISTS public.profile(
-	userid bigint PRIMARY KEY,
-	xp bigint,
-	money bigint,
-	gems integer,
-	tiles integer DEFAULT 2,
-	factoryslots integer DEFAULT 1,
+    userid bigint PRIMARY KEY,
+    xp bigint,
+    money bigint,
+    gems integer,
+    tiles integer DEFAULT 2,
+    factoryslots integer DEFAULT 1,
     factorylevel integer DEFAULT 0,
-	storeslots integer DEFAULT 1,
-	faction smallint,
+    storeslots integer DEFAULT 1,
+    faction smallint,
     notifications boolean DEFAULT true,
     registration_date date DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE IF NOT EXISTS public.inventory(
-	id SERIAL PRIMARY KEY,
-	userid bigint NOT NULL,
-	itemid smallint NOT NULL,
-	amount integer NOT NULL
+	id SERIAL,
+    userid bigint NOT NULL,
+    itemid smallint NOT NULL,
+	amount integer NOT NULL,
+    PK_item_user PRIMARY KEY (userid, itemid)
 );
 
 ALTER TABLE ONLY public.inventory
     ADD CONSTRAINT inventory_user_fkey FOREIGN KEY (userid) REFERENCES public.profile(userid) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS public.planted(
-	id SERIAL PRIMARY KEY,
-	itemid smallint NOT NULL,
-	userid bigint NOT NULL,
-	amount integer NOT NULL,
-	iterations smallint,
-	fieldsused smallint DEFAULT 1, 
-	ends timestamp NOT NULL,
-	dies timestamp NOT NULL,
-	robbedfields smallint DEFAULT 0,
+    id SERIAL PRIMARY KEY,
+    itemid smallint NOT NULL,
+    userid bigint NOT NULL,
+    amount integer NOT NULL,
+    iterations smallint,
+    fieldsused smallint DEFAULT 1, 
+    ends timestamp NOT NULL,
+    dies timestamp NOT NULL,
+    robbedfields smallint DEFAULT 0,
     catboost boolean DEFAULT false
 );
 
@@ -45,11 +46,11 @@ ALTER TABLE ONLY public.planted
     ADD CONSTRAINT planted_user_fkey FOREIGN KEY (userid) REFERENCES public.profile(userid) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS public.factory(
-	id SERIAL PRIMARY KEY,
-	userid bigint NOT NULL,
-	itemid smallint NOT NULL,
-	starts timestamp NOT NULL,
-	ends timestamp NOT NULL
+    id SERIAL PRIMARY KEY,
+    userid bigint NOT NULL,
+    itemid smallint NOT NULL,
+    starts timestamp NOT NULL,
+    ends timestamp NOT NULL
 );
 
 ALTER TABLE ONLY public.factory
@@ -69,12 +70,12 @@ ALTER TABLE ONLY public.missions
     ADD CONSTRAINT missions_user_fkey FOREIGN KEY (userid) REFERENCES public.profile(userid) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS public.store(
-	id SERIAL PRIMARY KEY,
-	guildid bigint NOT NULL,
-	userid bigint NOT NULL,
-	itemid smallint NOT NULL,
-	amount integer NOT NULL,
-	price integer NOT NULL,
+    id SERIAL PRIMARY KEY,
+    guildid bigint NOT NULL,
+    userid bigint NOT NULL,
+    itemid smallint NOT NULL,
+    amount integer NOT NULL,
+    price integer NOT NULL,
     username text NOT NULL
 );
 
@@ -82,11 +83,11 @@ ALTER TABLE ONLY public.store
     ADD CONSTRAINT store_user_fkey FOREIGN KEY (userid) REFERENCES public.profile(userid) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS public.boosts(
-	userid bigint PRIMARY KEY,
-	dog1 timestamp,
-	dog2 timestamp,
-	dog3 timestamp,
-	cat timestamp
+    userid bigint PRIMARY KEY,
+    dog1 timestamp,
+    dog2 timestamp,
+    dog3 timestamp,
+    cat timestamp
 );
 
 ALTER TABLE ONLY public.boosts
