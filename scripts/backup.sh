a88efdcc78e8b6f8b3372fe4436d181af7da2316
fuzzysearch404 <roberts.ziedins@inbox.lv>
2021-07-02 02:25:21 +0300

    Update database backup script to store backups in google drive folders per month days

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,18 +7,19 @@
 # $3 = Postgres username
 # $4 = Postgres password
 
-# Just in case the folder does not exist
+# Just in case the local folder does not exist
 mkdir backups
 
 BACKUP_FILE_NAME="backups/dump_`date +%H`.gz"
+BACKUP_FOLDER="dfarm_backups/`date +%d`"
 
 # Limit the dumps per hours. Btw: PGPASSWORD is a bad practice.
 PGPASSWORD="$4" pg_dump -w -c -U "$3" -h "$1" "$2" | gzip > $BACKUP_FILE_NAME
 unset PGPASSWORD
 
-# Overwrite the last day's hourly backup. 
+# Overwrite the current last month day's hourly backup.
 # gupload does not have any other file deletion options and I don't need my
 # google drive full of old backups... (I hope so)
-gupload $BACKUP_FILE_NAME -c dfarm_backups --overwrite
+gupload $BACKUP_FILE_NAME -c $BACKUP_FOLDER --overwrite
 
 exit 0
